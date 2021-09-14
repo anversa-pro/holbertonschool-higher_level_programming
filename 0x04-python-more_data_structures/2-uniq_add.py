c0c22511c7aa44839f0574408d41584277a93354
anversa-pro <angelavergaras@gmail.com>
2021-09-14 15:27:28 -0500

    [MODIFY] add empty list case

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,8 @@
 #!/bin/usr/python3
 def uniq_add(my_list=[]):
-    return(sum(set(my_list)))
+    result = 0
+    if my_list is None:
+        return (result)
+    for i in set(my_list):
+        result += i
+    return(result)
