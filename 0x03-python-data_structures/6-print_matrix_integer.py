0899e28a7e2bdbe78e3274ffc34c4ac773bfe61a
anversa-pro <angelavergaras@gmail.com>
2021-09-13 20:36:56 -0500

    [MODIFY] Change loop logic

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for value in row:
-            print(value, end=" ")
-        print()
+            print(' '.join('{:d}'.format(value) for value in row))
