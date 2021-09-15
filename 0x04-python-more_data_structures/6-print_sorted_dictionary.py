fdbb17fd27f33cda946baff6a743892e907cc1f6
anversa-pro <angelavergaras@gmail.com>
2021-09-14 21:40:38 -0500

    [MODIFY] add space before keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     ordered_keys = {}
     for i in sorted(a_dictionary):
         ordered_keys[i] = a_dictionary[i]
-        print("{}:{}".format(i, ordered_keys[i]))
+        print("{}: {}".format(i, ordered_keys[i]))
