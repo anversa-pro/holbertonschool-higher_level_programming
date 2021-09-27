e8088ceaad1ad2d74856c49e9220ecf02dfdb384
anversa-pro <angelavergaras@gmail.com>
2021-09-27 17:34:26 -0500

    [MODIFY] change format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in my_list[:x]:
             if isinstance(i, int):
-                print("{}".format(i), end="")
+                print("{:d}".format(i), end="")
                 printed += 1
         print()
     except:
