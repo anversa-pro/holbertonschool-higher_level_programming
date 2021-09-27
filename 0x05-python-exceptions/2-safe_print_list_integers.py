a0e2f8b7174981cf388df281de124803ff143c78
anversa-pro <angelavergaras@gmail.com>
2021-09-27 17:51:39 -0500

    [MODIFY] change loop location

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     printed = 0
-    try:
-        for i in my_list[:x]:
-            if isinstance(i, int):
-                print("{:d}".format(i), end="")
-                printed += 1
-        print()
-    except:
-        continue
-    return(printed)
+    for i in my_list[:x]:
+        try:
+            print("{:d}".format(i), end="")
+            printed += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return printed
