a7f51549bfde778610fa9056bf584b4dfa6fd772
anversa-pro <angelavergaras@gmail.com>
2021-09-27 18:25:26 -0500

    [MODIFY] rollback

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,5 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
             printed += 1
         except (ValueError, TypeError):
             continue
-        print()
+    print()
     return printed
