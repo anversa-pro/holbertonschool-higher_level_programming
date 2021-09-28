4b7e38397fac0f910ee384d21b97911227d73b65
anversa-pro <angelavergaras@gmail.com>
2021-09-27 19:09:53 -0500

    [MODIFY] change loop condition

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     i = printed = 0
-    for i in my_list[:x]:
+    for i in range (0, x):
         try:
-            print("{:d}".format(i), end="")
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
         except (ValueError, TypeError):
             continue
