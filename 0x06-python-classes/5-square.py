4d9a01b4b3c89bcaa4ddb62be0ddf5cbb7c1d179
anversa-pro <angelavergaras@gmail.com>
2021-09-29 17:09:23 -0500

    [CREATE] file to print squares

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square and checks if is an integer size"""
+
+
+class Square:
+    """Class Square definition"""
+    pass
+
+    def __init__(self, size=0):
+        """init method of class Square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
