d43173011e82dfbe903372ad21b2caee6c330257
anversa-pro <angelavergaras@gmail.com>
2021-09-29 15:24:24 -0500

    [CREATE] a class & method to print a size

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square and checks if is an integer size"""
+
+
+class Square:
+    """Class Square definition"""
+    pass
+
+    def __init__(self, size=0):
+        """init method of class Square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+	def area(self):
+        """method that returns the current square area"""
+        return (self.__size * self.__size)
