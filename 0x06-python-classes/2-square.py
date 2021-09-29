1078765a6233fe01b32ad09efaac0b337e2ebb9e
anversa-pro <angelavergaras@gmail.com>
2021-09-29 12:44:48 -0500

    [MODIFY] fix identation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,15 @@
 
 """class Square that defines a square and checks if is an integer size"""
 
+
 class Square:
     """Class Square definition"""
     pass
 
-def __init__(self, size=0):
-    """init method of class Square"""
-    if type(size) != int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    def __init__(self, size=0):
+        """init method of class Square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
