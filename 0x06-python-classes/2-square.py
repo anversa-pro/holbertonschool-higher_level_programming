1a79b44960cf56d33fc3a392b546eed058b980aa
anversa-pro <angelavergaras@gmail.com>
2021-09-29 12:36:30 -0500

    [CREATE] add documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+"""class Square that defines a square and checks if is an integer size"""
 
 class Square:
-    '''Class Square object intialized with size '''
+    """Class Square definition"""
     pass
 
 def __init__(self, size=0):
-    '''init method of class Square '''
+    """init method of class Square"""
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
