8ffde9d64c7cf0d85709eb954e65c96bc985f920
anversa-pro <angelavergaras@gmail.com>
2021-10-01 15:17:45 -0500

    [CREATE] file to count instances

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,65 @@
+ #!/usr/bin/python3
+
+""" class Rectangle that defines a rectangle attributes """
+
+
+class Rectangle:
+    """ class Rectangle with private instance attributes """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """init method of class Square"""
+        self.width = width
+        self.height = height
+        self.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        calculated_str = ""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        for idx in range(self.__height):
+            calculated_str += '#' * self.width
+            if idx + 1 < self.__height:
+                calculated_str += '\n'
+        return calculated_str
+
+    def __repr__(self):
+        calculated_str = "Rectangle(" + str(self.__width) + ","
+        calculated_str += str(self.__height) + ")"
+        return calculated_str
+
+    def __del__(self):
+        Rectangle.number_of_instances -=1
+        print("Bye rectangle...")
