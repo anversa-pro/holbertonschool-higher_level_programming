8d1e5ff0cf6d827a2b25e3d20bad43564cc03b72
anversa-pro <angelavergaras@gmail.com>
2021-10-01 15:32:10 -0500

    [MODIFY] loop declaration

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -47,9 +47,10 @@ class Rectangle:
         calculated_str = ""
         if self.__height == 0 or self.__width == 0:
             return ""
-        if self.__width != 0 and self.__height != 0:
-            calculated_str += "\n".join("#" * self.__width
-                for i in range(self.__height))
+        for idx in range(self.__height):
+            calculated_str += '#' * self.width
+            if idx + 1 < self.__height:
+                calculated_str += '\n'
         return calculated_str
 
     def __repr__(self):
