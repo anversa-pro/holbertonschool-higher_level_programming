50aeb226c43fbfcda32716e72858e99592b7f361
anversa-pro <angelavergaras@gmail.com>
2021-10-01 15:27:52 -0500

    [MODIFY] fix method assignation

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """init method of class Square"""
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
