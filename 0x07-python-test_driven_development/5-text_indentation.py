1bf5393fd68f36db13d5ead4701b33c231fb4496
anversa-pro <angelavergaras@gmail.com>
2021-10-06 18:10:34 -0500

    [CREATE] file to test indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
+
+""" Defines a text-indentation function."""
+
+
 def text_indentation(text):
-	if type(text) is not str:
-		raise TypeError("text must be a string")
-	for i in text:
-		print("{:s}".format(i),end="")
-		if i == '.' or i == ':':
-			print("\n")
+    """
+    Print text with two new lines after each '.', '?', and ':'.
+
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
