daa12f6ad8f44605e5757d52881e704bfc28ac63
anversa-pro <angelavergaras@gmail.com>
2021-10-07 00:15:13 -0500

    [MODIFY] documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+Module to add 2 numbers
+Arg: number a and number b
+Return: Result of adding
+
+"""
+
+
 def add_integer(a, b=98):
+    """
+    function that validates integer args and calculate the operation
+    Arg: number a and number b
+    Return: Result of adding
+
+    """
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
