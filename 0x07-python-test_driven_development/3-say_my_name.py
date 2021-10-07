3d3ace958ac11c3c79f60da547cc60391618e5ff
anversa-pro <angelavergaras@gmail.com>
2021-10-06 23:57:48 -0500

    [MODIFY] pep & style

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+Module to print a name
+Arg: first & last name
+Return: None
+
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Module to find print a name
+    Arg: List
+    Return: Max integer in a given list
+
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
