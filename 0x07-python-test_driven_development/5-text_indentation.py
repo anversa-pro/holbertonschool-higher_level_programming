9cce2bcddccb22394c180c86773357c106253cf4
anversa-pro <angelavergaras@gmail.com>
2021-10-06 23:43:17 -0500

    [MODIFY] Fix style

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+"""
+Defines a text-indentation function.
+Arg: String
+Return: None
 
-""" Defines a text-indentation function."""
+"""
 
 
 def text_indentation(text):
     """
     Print text with two new lines after each '.', '?', and ':'.
+    Deletes spaces after characters
 
     """
 
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    iterator = 0
+    while iterator < len(text) and text[iterator] == ' ':
+        iterator += 1
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
+    while iterator < len(text):
+        print(text[iterator], end="")
+        if text[iterator] == "\n" or text[iterator] in ".?:":
+            if text[iterator] in ".?:":
                 print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
+            iterator += 1
+            while iterator < len(text) and text[iterator] == ' ':
+                iterator += 1
             continue
-        c += 1
+        iterator += 1
