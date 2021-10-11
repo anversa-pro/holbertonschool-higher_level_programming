461bb918b6ddd4596451ed7995bdad490e1a0e7d
anversa-pro <angelavergaras@gmail.com>
2021-10-11 18:52:08 -0500

    [CREATE] file to verify inherited

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that compares an instance of the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False.
+    """
+    if ((isinstance (obj, a_class))):
+        return(True)
+    else:
+        return(False)
