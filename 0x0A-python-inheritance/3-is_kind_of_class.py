3a7314fda72aab3ca5baade59b7ab2c21e68b9ea
anversa-pro <angelavergaras@gmail.com>
2021-10-11 18:54:17 -0500

    [MODIFY]fix pep8

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -10,7 +10,7 @@ def is_kind_of_class(obj, a_class):
     or if the object is an instance of a class that inherited from,
     the specified class ; otherwise False.
     """
-    if ((isinstance (obj, a_class))):
+    if ((isinstance(obj, a_class))):
         return(True)
     else:
         return(False)
