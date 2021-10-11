2fb7f7f74eff95c27506971403dfb161d85ca9ab
anversa-pro <angelavergaras@gmail.com>
2021-10-11 18:40:00 -0500

    [MODIFY] fix pep8

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,9 +6,10 @@ function that compares an instance of the specified class
 
 def is_same_class(obj, a_class):
     """
-    function that returns True if the object is exactly an instance of the specified class; otherwise False.
+    function that returns True if the object
+    is exactly an instance of the specified class; otherwise False.
     """
-    if  (type(obj) == a_class):
+    if (type(obj) == a_class):
         return(True)
     else:
         return(False)
