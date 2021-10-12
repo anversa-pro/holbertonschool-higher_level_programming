0b4088373a54250be0b1e1d5302593cb51c707ca
anversa-pro <angelavergaras@gmail.com>
2021-10-11 20:48:02 -0500

    [CREATE] Class constructor

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Create a subclass
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Subclass Rectangle inheritated of BaseGometry
+    """
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
