f185d889c1feb9ed69238989ee36daeb23225202
anversa-pro <angelavergaras@gmail.com>
2021-10-11 22:15:47 -0500

    [MODIFY] value

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,9 +11,6 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self.integer_validator("width", size)
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-    
-    def area(self):
-        return self.__size * self.__size
