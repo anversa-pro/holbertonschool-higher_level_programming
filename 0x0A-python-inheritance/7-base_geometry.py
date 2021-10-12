263a313d3449c38266b86e8ef5c82808648385fa
anversa-pro <angelavergaras@gmail.com>
2021-10-11 20:34:11 -0500

    [CREATE] file that rises typeError & ValueError

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Create a class
+"""
+
+
+class BaseGeometry():
+    """
+    class BaseGeometry
+    """
+    pass
+
+    def area(self):
+        """
+        That raises an Exception with a message
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates value
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer". format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0". format(name))
