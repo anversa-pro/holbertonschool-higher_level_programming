f411f25d4f39ebc26297d1074266a2ac067aba53
anversa-pro <angelavergaras@gmail.com>
2021-10-11 20:17:41 -0500

    [CREATE] Class method with exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Create an class
+"""
+
+
+class BaseGeometry():
+    """
+    class MyList that inherits from list
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
