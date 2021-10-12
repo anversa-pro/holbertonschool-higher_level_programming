c6533e5431282fbe95049c8f59c93a1d8610948a
anversa-pro <angelavergaras@gmail.com>
2021-10-11 23:01:52 -0500

    [MODIFY] change validation

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,7 +20,8 @@ class BaseGeometry():
         """
         Public instance method that validates value
         """
-        if type(value) is not int:
+        if isinstance(value, int) is False:
             raise TypeError("{} must be an integer". format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0". format(name))
