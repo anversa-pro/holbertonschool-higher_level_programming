3b16b2d42d940a1814ea65495e52664b5728be2e
anversa-pro <angelavergaras@gmail.com>
2021-10-13 17:00:19 -0500

    [CREATE] file to create a class and retrive it

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that create class Student that defines a student
+"""
+
+
+class Student():
+    """
+    Function that create class Student that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Method that retrieves a dictionary representation
+        """
+        return self.__dict__
