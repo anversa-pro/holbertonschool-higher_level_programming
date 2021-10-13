f9974df26fecae67a5a9a21ea4fe47c1308f2ca5
anversa-pro <angelavergaras@gmail.com>
2021-10-12 20:39:41 -0500

    [CREATE] file to print an utf file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that reads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Function reads a text file (UTF8) and prints it to stdout
+    Returns none
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
