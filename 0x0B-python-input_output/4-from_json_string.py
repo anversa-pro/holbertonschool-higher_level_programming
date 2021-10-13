16e5fcbc80695e95527685238c4bec66f58843fa
anversa-pro <angelavergaras@gmail.com>
2021-10-12 21:14:41 -0500

    [MODIFY] pep8

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-File that returns an object (Python data structure) 
+File that returns an object (Python data structure)
 represented by a JSON string
 """
 import json
 
 
 def from_json_string(my_str):
-    """ 
-    Function that returns an object (Python data structure) 
+    """
+    Function that returns an object (Python data structure)
     represented by a JSON string:
     args:
         my_str: object to be return as JSON string
