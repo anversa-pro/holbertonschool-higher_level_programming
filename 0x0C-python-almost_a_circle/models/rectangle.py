624f14f4ae057b77f949ee2b0e961ee1bc63c41a
anversa-pro <angelavergaras@gmail.com>
2021-10-17 16:23:06 -0500

    [UPDATE] add methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"Class to define a rectangle"
+""" Class to define a rectangle """
 from models.base import Base
 
+
 class Rectangle (Base):
-    "PENDING COMMETS"
+    """ Defines a rectangle representation that inherits of Base class """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
@@ -17,6 +18,10 @@ class Rectangle (Base):
 
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -25,20 +30,62 @@ class Rectangle (Base):
 
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
-    def x(self, value):
+    def x(self):
         return self.__x
 
     @x.setter
     def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
-    def y(self, value):
+    def y(self):
         return self.__y
 
     @y.setter
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ Calculate a rectangle area (w * h) and return it """
+        return (self.width * self.height)
+
+    def display(self):
+        """ prints a representation of the square by # """
+        print('\n' * self.y, end='')
+        for counter in range(self.height):
+            print(' ' * self.x, end='')
+            print('#' * self.width, end='')
+            if counter + 1 < self.height:
+                print()
+        print()
+
+    def __str__(self):
+        txt = "[Rectangle] ({}) {}/{} - {}/{}"
+        return txt.format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        attributesList = ['id', 'width', 'height', 'x', 'y']
+        if args is not None and len(args) > 0:
+            for i, argument in enumerate(args):
+                if i < len(attributesList):
+                    setattr(self, attributesList[i], argument)
+
+        elif kwargs is not None and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key in attributesList:
+                    setattr(self, key, value)
