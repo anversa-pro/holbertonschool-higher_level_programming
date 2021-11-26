b521f0530f4c532c09575c987a51e4da777635bd
anversa-pro <angelavergaras@gmail.com>
2021-11-25 19:59:38 -0500

    [MODIFY] change table name

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of a table in a database in relative MySQL server.
-SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
