71d1e949420cc14636f4f14f44ff31614a42765b
anversa-pro <angelavergaras@gmail.com>
2021-12-15 18:34:43 -0500

    [CREATE] file do add 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Script that prints the addition of 2 integers
+*/
+const firstNumber = parseInt(process.argv[2]);
+const secondNumber = parseInt(process.argv[3]);
+
+if (firstNumber && secondNumber) {
+  add(firstNumber, secondNumber);
+  } else{
+      console.log(secondNumber)
+}
+function add(a, b){
+    const addition = a + b;
+    console.log(addition);
+}
