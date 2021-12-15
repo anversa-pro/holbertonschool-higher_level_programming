f97e30f63bcff0d5097295ce7584c946da0b426b
anversa-pro <angelavergaras@gmail.com>
2021-12-14 23:58:04 -0500

    [CREATE] fie to print a message with a condition

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Script that prints a message depending of the number of arguments
+*/
+if (process.argv.length === 2) {
+    console.log('No argument');
+}
+else if (process.argv.length === 3) {
+    console.log('Argument found');
+}
+else if(process.argv.length > 3) {
+    console.log('Arguments found');
+}
