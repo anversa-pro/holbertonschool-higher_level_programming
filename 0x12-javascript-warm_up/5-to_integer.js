075036808a294a3dd6c3b7b29758676a481a48af
anversa-pro <angelavergaras@gmail.com>
2021-12-15 15:30:46 -0500

    [CREATE] file to print an argument if it's a number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+Script script that prints My number 
+if the first argument can be converted to an integer:
+*/
+if (!parseInt(process.argv[2])) {
+    console.log('Not a number');
+} else {
+    console.log('My number: ' + parseInt(process.argv[2]));
+}
+  
