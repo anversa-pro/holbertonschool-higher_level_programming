28717abb748a3d23ed765dfbd0425590f0376592
anversa-pro <angelavergaras@gmail.com>
2021-12-15 15:33:30 -0500

    [MODIFY] fix standard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 /*
-Script script that prints My number 
+Script script that prints My number
 if the first argument can be converted to an integer:
 */
 if (!parseInt(process.argv[2])) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
-  
