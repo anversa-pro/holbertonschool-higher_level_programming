fc4e668eefa0697e8cbaa943c75adf1d090d68d5
anversa-pro <angelavergaras@gmail.com>
2021-12-14 23:47:26 -0500

    [CREATE] file that print specific lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+Script that prints 3 lines
+*/
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
