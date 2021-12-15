61932c7e0524bc7964b0930a2b0a151fd31ca517
anversa-pro <angelavergaras@gmail.com>
2021-12-15 00:00:27 -0500

    [MMODIFY] change order & fix style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,11 +3,9 @@
 Script that prints a message depending of the number of arguments
 */
 if (process.argv.length === 2) {
-    console.log('No argument');
-}
-else if (process.argv.length === 3) {
-    console.log('Argument found');
-}
-else if(process.argv.length > 3) {
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
