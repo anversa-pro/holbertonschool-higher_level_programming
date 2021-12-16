d378fa044338a718d6e16d7360367e4734512882
anversa-pro <angelavergaras@gmail.com>
2021-12-16 13:36:12 -0500

    [MODIFY] fix standard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,11 +7,11 @@ console.log(factorialCalculation(factorialNumber));
 
 function factorialCalculation (a) {
   let calculation = 1;
-  if (a === 0 || isNaN(a)){
-      return(1);
+  if (a === 0 || isNaN(a)) {
+    return (1);
   }
   for (let i = 1; i <= a; i++) {
     calculation *= i;
   }
-  return(calculation)
+  return (calculation);
 }
