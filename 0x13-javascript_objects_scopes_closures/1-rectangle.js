522e8b7c40bf6bd71db481de7083b29f12a1275d
anversa-pro <angelavergaras@gmail.com>
2021-12-16 23:01:26 -0500

    [CREATE] file to set a rectangle &construuctor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+Empty class Rectangle that defines a rectangle
+The constructor takes 2 arguments
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
