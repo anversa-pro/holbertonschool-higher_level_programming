bd370ba0e8862f384feda8dacdbc7ed71d79d04b
anversa-pro <angelavergaras@gmail.com>
2022-01-09 20:25:33 +0000

    [MODIFY] reduce to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-TOURL=$1
-curl -sI "TOURL" | grep Content-Length: | cut -d " " -f 2
+curl -sI "$1" | grep Content-Length: | cut -d " " -f 2
