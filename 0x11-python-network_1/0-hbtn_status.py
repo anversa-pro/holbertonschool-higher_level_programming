f515b5b46654300cbeeb399fa7856fa9722f74ed
anversa-pro <angelavergaras@gmail.com>
2022-01-10 02:10:35 +0000

    [CREATE] file to print a body response

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module contains a script to fetch a URL """
+import urllib.request
+
+
+if __name__ == "__main__":
+    """
+    This script fetches a domain using urllib 
+    and displays the body of the response
+    """
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+        information = request.read()
+        print("Body response:")
+        print("\t- type:", type(information))
+        print("\t- content:", information)
+        print("\t- utf8 content:", str(information)[2:-1])
