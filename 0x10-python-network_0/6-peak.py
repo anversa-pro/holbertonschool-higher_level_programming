24175348d1607fa63da1fd7d9183d93b3829d883
anversa-pro <angelavergaras@gmail.com>
2022-01-10 01:29:38 +0000

    [MODIFY] change complexity and efficient function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,5 +9,5 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or len(list_of_integers) < 1:
         return None
-    maximum = max(list_of_integers)
-    return maximum
+    list_of_integers.sort()
+    return list_of_integers[-1]
