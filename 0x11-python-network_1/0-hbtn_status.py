1fef69242f0d502ec680bc6632a3231a1443380f
anversa-pro <angelavergaras@gmail.com>
2022-01-10 02:31:58 +0000

    [MODIFY] delete changes considering CHECKER errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" Script to fetch https://intranet.hbtn.io/status
-    and display the status
+"""
+Script that fetches https://intranet.hbtn.io/status
+and displays status information
 """
 import urllib.request
 
 
 if __name__ == "__main__":
     """
-    This script fetches a domain using urllib
-    and displays the body of the response
+    This script fetches a URL with an urllib
+    and displays info of the status
     """
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-        statusInfo = request.read()
-        print("Body response:")
-        print("\t- type:", type(statusInfo))
-        print("\t- content:", statusInfo)
-        print("\t- utf8 content:", str(statusInfo)[2:-1])
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        statusInfo = response.read()
+
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(statusInfo), statusInfo, str(statusInfo)[2:-1]))
