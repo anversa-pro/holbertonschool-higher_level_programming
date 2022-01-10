e28de18f4ce4388e3e454eeb6c58c03d68c8a390
anversa-pro <angelavergaras@gmail.com>
2022-01-10 00:17:48 +0000

    [MODIFY] fix indentation w

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" function that finds a peak in a list of unsorted integers.
+""" Function that finds a peak in a list of unsorted integers.
     A Maximum is a peak.
 """
 
 
 def find_peak(list_of_integers):
-""" retuns a peak number from a unsorted list"""
+    """ Returns a peak number from a unsorted list"""
 
     if list_of_integers is None or len(list_of_integers) < 1:
         return None
