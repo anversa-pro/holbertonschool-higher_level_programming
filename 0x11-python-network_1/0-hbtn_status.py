3ee20620dec4503f6b22b008a2a819e29eb56d2d
anversa-pro <angelavergaras@gmail.com>
2022-01-10 02:28:15 +0000

    [MODIFY] rollback to previous printing status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-and displays status information of the body response
+""" Script to fetch https://intranet.hbtn.io/status
+    and display the status
 """
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        statusInfo = response.read()
-
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(statusInfo), statusInfo, str(statusInfo)[2:-1]))
+    """
+    This script fetches a domain using urllib
+    and displays the body of the response
+    """
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+        statusInfo = request.read()
+        print("Body response:")
+        print("\t- type:", type(statusInfo))
+        print("\t- content:", statusInfo)
+        print("\t- utf8 content:", str(statusInfo)[2:-1])
