198c79899cc0db50b03a4315090770b8a5492cd9
anversa-pro <angelavergaras@gmail.com>
2022-01-10 01:37:07 +0000

    [MODIFY] shorten conditional

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,7 @@
 def find_peak(list_of_integers):
     """ Returns a peak number from a unsorted list"""
 
-    if list_of_integers is None or len(list_of_integers) < 1:
+    if len(list_of_integers) < 1:
         return None
     list_of_integers.sort()
     return list_of_integers[-1]
