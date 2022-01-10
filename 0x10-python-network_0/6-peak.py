80ff728a605d2086888774d91aef027addcaa289
anversa-pro <angelavergaras@gmail.com>
2022-01-10 00:08:14 +0000

    [CREATE] file to find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function to find a peak """
+
+def find_peak(list_of_integers):
+
+    if len(list_of_integers) < 2:
+        return None
+    maximum = max(list_of_integers)
+        return maximum
