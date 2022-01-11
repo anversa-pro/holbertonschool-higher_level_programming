118fbe8c3b45e845897735aaa23fce418234aaba
anversa-pro <angelavergaras@gmail.com>
2022-01-11 00:59:29 +0000

    [CREATE] file to display a parameter with requests lib

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays the value
+of the variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """
+    This script fetches a URL with an urllib
+    and displays info of the status
+    """
+    URL = sys.argv[1]
+    response = requests.get(URL)
+    response = response.headers.get("X-Request-Id")
+    print(response)
