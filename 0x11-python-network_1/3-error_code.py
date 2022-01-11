765944ebc540e340005f15d2182a7209cbbd6884
anversa-pro <angelavergaras@gmail.com>
2022-01-11 00:35:00 +0000

    [CREATE] file to send a request tu an URL

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL and displays the body 
+of the response (decoded in utf-8) and an error message if needed
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            answer = response.read().decode('utf-8')
+            print(answer)
+    except urllib.error.HTTPError as Error:
+        print("Error code: {}".format(Error.code))
