ed13fe878cdfd0d4f5795ae8cf7fa68b1da56c4f
anversa-pro <angelavergaras@gmail.com>
2022-01-11 01:21:16 +0000

    [CREATE] file to send a post request and display a body

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email address, sends a POST request
+with the email as a parameter, and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Displays the body of the response
+    """
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
