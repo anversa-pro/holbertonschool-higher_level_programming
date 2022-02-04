f95ae50d8bfb43320e7adc8a861c83186c4ca1e8
anversa-pro <angelavergaras@gmail.com>
2022-02-04 05:05:32 +0000

    [MODIFY] change conditional

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,13 +3,13 @@ const squaresix = require('./5-square');
 
 class Square extends squaresix {
   charPrint (c) {
-    if (!c) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.width; i++) {
-      console.log(c.repeat(this.width));
+    if (c) {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
     }
   }
 }
-
 module.exports = Square;
